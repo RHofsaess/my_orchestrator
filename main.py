ec1081381139b5b9260e3412672ee81e8ae344bc
Robin Hofsaess <Robin.Hofsaess@gmx.de>
2025-05-26 09:46:50 +0200

    Adding python version check -- > 3.6 is required!

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import sys
+
+if not (sys.version_info.major == 3 and sys.version_info.minor > 6):
+    raise Exception("Python version must be greater than 3.6")
+
 from cli import *
 # import interactive # TODO
 
